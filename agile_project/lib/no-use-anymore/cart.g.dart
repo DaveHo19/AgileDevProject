0a07936a9af9bbde8a9e76b7682537506a85c13f
hoteikwei96 <hoteikwei96@users.noreply.github.com>
2022-07-03 16:17:37 +0800

    merged and modified some file naming

--- agile_project/lib/no-use-anymore/cart.g.dart
+++ agile_project/lib/no-use-anymore/cart.g.dart
@@ -6,43 +6,43 @@ part of 'cart.dart';
 // TypeAdapterGenerator
 // **************************************************************************
 
-class CartAdapter extends TypeAdapter<Cart> {
+class CartAdapter /* extends TypeAdapter<Cart> */ {
   @override
   final int typeId = 0;
 
-  @override
-  Cart read(BinaryReader reader) {
-    final numOfFields = reader.readByte();
-    final fields = <int, dynamic>{
-      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
-    };
-    return Cart(
-      ISBN_13: fields[0] as String,
-      title: fields[1] as String,
-      imageCoverURL: fields[2] as String,
-      retailPrice: fields[3] as double,
-      quantity: fields[4] as int,
-      cartQuantity: fields[5] as int,
-    );
-  }
+  // @override
+  // Cart read(BinaryReader reader) {
+  //   final numOfFields = reader.readByte();
+  //   final fields = <int, dynamic>{
+  //     for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+  //   };
+  //   return Cart(
+  //     ISBN_13: fields[0] as String,
+  //     title: fields[1] as String,
+  //     imageCoverURL: fields[2] as String,
+  //     retailPrice: fields[3] as double,
+  //     quantity: fields[4] as int,
+  //     cartQuantity: fields[5] as int,
+  //   );
+  // }
 
-  @override
-  void write(BinaryWriter writer, Cart obj) {
-    writer
-      ..writeByte(6)
-      ..writeByte(0)
-      ..write(obj.ISBN_13)
-      ..writeByte(1)
-      ..write(obj.title)
-      ..writeByte(2)
-      ..write(obj.imageCoverURL)
-      ..writeByte(3)
-      ..write(obj.retailPrice)
-      ..writeByte(4)
-      ..write(obj.quantity)
-      ..writeByte(5)
-      ..write(obj.cartQuantity);
-  }
+  // @override
+  // void write(BinaryWriter writer, Cart obj) {
+  //   writer
+  //     ..writeByte(6)
+  //     ..writeByte(0)
+  //     ..write(obj.ISBN_13)
+  //     ..writeByte(1)
+  //     ..write(obj.title)
+  //     ..writeByte(2)
+  //     ..write(obj.imageCoverURL)
+  //     ..writeByte(3)
+  //     ..write(obj.retailPrice)
+  //     ..writeByte(4)
+  //     ..write(obj.quantity)
+  //     ..writeByte(5)
+  //     ..write(obj.cartQuantity);
+  // }
 
   @override
   int get hashCode => typeId.hashCode;
